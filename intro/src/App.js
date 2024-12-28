4b2b4687f304919f91afb50ea875cb784204b01e
farahhowari <farahhowari783@gmail.com>
2024-12-28 17:48:44 +0300

    new react

---
+++ intro/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './componants/navBar';
+function App() {
+  return (
+    <div className="App">
+     
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
